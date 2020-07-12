d4b4a04847774b4141bfd7923f0a430e691397a0
Gonza Ferretti <gonzalofe1998@hotmail.com>
2020-07-12 12:12:37 -0300

    added color lerp to hp bar

--- Game/Space Jalopy/Assets/Scripts/UI/HpBar.cs
+++ Game/Space Jalopy/Assets/Scripts/UI/HpBar.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class HpBar : MonoBehaviour
 {
     public Image barFill;
+    public Color emptyColor;
+    public Color fullColor;
     public void ModifyHP(float amount)
     {
         barFill.fillAmount = amount;
+        barFill.color = Color.Lerp(emptyColor, fullColor, amount);
     }
 }
