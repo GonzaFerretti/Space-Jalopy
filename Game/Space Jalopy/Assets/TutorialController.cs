e2afd5f90bd558ddb77182225114be126b876bc6
Aiden Aspen <aiden.aspen99@gmail.com>
2020-07-12 04:07:35 -0300

    Tutorial

---
+++ Game/Space Jalopy/Assets/TutorialController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    private Dictionary<KeyCode, bool> _movementKeys = new Dictionary<KeyCode, bool>()
+    {
+        { KeyCode.W, false },
+        { KeyCode.A, false },
+        { KeyCode.S, false },
+        { KeyCode.D, false },
+        { KeyCode.Space, false }
+    };
+
+    private int _tutorialStep = 1;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        bool allKeysTouched = true;
+        for (int i = 0; i < _movementKeys.Count; i++)
+        {
+            allKeysTouched = allKeysTouched && _movementKeys.ElementAt(i).Value;
+        }
+
+        if (allKeysTouched && _tutorialStep == 1)
+        {
+            _tutorialStep = 2;
+        }
+    }
+}
