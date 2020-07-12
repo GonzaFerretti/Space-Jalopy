bbd83884cbf9a21d326af5d7960348e0b6604936
Gonza Ferretti <gonzalofe1998@hotmail.com>
2020-07-12 12:31:44 -0300

    removed hooks after killing boss ship

--- Game/Space Jalopy/Assets/Scripts/Characters/Enemies/BossShip.cs
+++ Game/Space Jalopy/Assets/Scripts/Characters/Enemies/BossShip.cs
@@ -4,4 +4,23 @@ using UnityEngine;
 
 public class BossShip : ChampionShip
 {
+    public override void ApplyDamage(int damage)
+    {
+        if (currentHp - damage <= 0)
+        {
+            try
+            {
+                foreach (HookProjectile hook in FindObjectsOfType<HookProjectile>())
+                {
+                    Destroy(hook.gameObject);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        base.ApplyDamage(damage);
+
+    }
 }
