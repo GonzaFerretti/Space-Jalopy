64d907ddd088bafc6e9d9edd6fbc3918428bf95e
Aiden Aspen <aiden.aspen99@gmail.com>
2020-07-12 01:35:36 -0300

    Splash

---
+++ Game/Space Jalopy/Assets/Scripts/FadeOutScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeOutScript : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+
+        Invoke("StartFadeOut", 3);
+    }
+
+    private void StartFadeOut()
+    {
+        StartCoroutine("FadeOut");
+    }
+
+    private IEnumerator FadeOut()
+    {
+        for(float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
---
+++ Game/Space Jalopy/Assets/Scripts/TextFadeOut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextFadeOut : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        Invoke("StartFadeOut", 3);
+    }
+
+    public void StartFadeOut()
+    {
+        StartCoroutine("FadeOutText");
+    }
+
+    private IEnumerator FadeOutText()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = text.color;
+            c.a = f;
+            text.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
